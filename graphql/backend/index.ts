201e624257ec75ca715f4a7f680b4cf1ed125970
Matheus Zago Bestana <matheusbestana@gmail.com>
2022-03-25 11:46:11 -0300

    Add files via upload

---
+++ graphql/backend/index.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "apollo-server";
+import path from "path";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./src/resolvers/UserResolver";
+
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+    emitSchemaFile: path.resolve(__dirname, "scheme.gql"),
+  });
+
+  const server = new ApolloServer({ schema });
+
+  const { url } = await server.listen();
+
+  console.log(`Server running on ${url}`);
+}
+
+main();
